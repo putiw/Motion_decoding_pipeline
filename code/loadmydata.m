e5b81ad12959a91a370d7d7a12e409aace9dc9ee
putiw <pw1246@nyu.edu>
2021-11-17 13:51:24 +0400

    load data based on run-info.txt

--- code/loadmydata.m
+++ code/loadmydata.m
@@ -1,34 +1,10 @@
 function [dataset stim_label] = loadmydata(BASE,sub,ses,run,roi,p)
 %
 % Load and process (detrend, normalize) data for classification
-%
-% Ultimately we want to return dataset as a cell array: sub x ses x run x
-% roi
 
-% subject scan # for each session
-scanID = repmat((1:10)',1,4);
-if sub(end) == '4' % this subject uses different scan index
-    scanID(:,1) = [(5:13)';15];
-    scanID(:,3) = [(2:11)'];
-end
+runfilename = [BASE 'derivatives/fmriprep/' sub '/' sub '_run-info'];
+runinfo = readtable(runfilename);
 
-% scan name
-
-switch sub
-    case {'sub-0203ny'}
-        taskname = 'TASK';
-    case {'sub-0204'}
-        taskname = 'TASK';
-    case {'sub-0205'}
-        taskname = 'TASK';
-    case {'sub-0206'}
-        taskname = 'TASK';
-    otherwise
-        taskname = '3dmotion';
-end
-
-
-           
 % Allocate data
 DATA = cell(numel(ses).*numel(run),numel(roi));
 stim_label=[];
@@ -38,11 +14,11 @@ stim = [[5:-1:1 8:-1:6]' [4:8 1:3]'];
 for whichSession = 1:numel(ses)
     for whichRun = 1:numel(run)
         
-        runidx = scanID(whichRun,str2double(ses(whichSession)));
-        RUN = num2str(runidx);
+        runidx = runinfo{whichRun,whichSession};
         
         datapath = [BASE,'derivatives/fmriprep/',sub,'/ses-',ses{whichSession},'/func/', ...
-            sub,'_ses-',ses{whichSession},'_task-',taskname,'_run-',RUN,'_space-T1w_desc-preproc_bold.nii.gz'];
+            sub,'_ses-',ses{whichSession},'_task-',char(runinfo{whichRun,6}),'_run-',num2str(runidx), ...
+            '_space-T1w_desc-preproc_bold.nii.gz'];
         
         disp(['Loading: ' datapath]);
         Func = niftiread(fullfile(datapath));
@@ -103,7 +79,8 @@ for whichSession = 1:numel(ses)
                             error('Unknown sample unit')
                     end
                     
-                    % output is downsampled from TRs (240) x voxels to average response per run per direction (8) x voxels
+                    % output is downsampled from TRs (240) x voxels to average response per run per direction (8) x voxels                    
+
                     DATA{numel(run)*(whichSession-1)+whichRun,whichRoi} = samples;
                 end % end of roi 
         end % end of run
